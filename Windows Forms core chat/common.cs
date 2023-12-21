92e357d5483199cb9215d9b82f9641d5104384e7
Ancol <trandieu@gmail.com>
2023-12-21 23:29:53 +0700

    add username

---
+++ Windows Forms core chat/common.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Windows_Forms_CORE_CHAT_UGH
+{
+    public class common
+    {
+        public const string USERNAME = "!username";
+        public const string COMMANDS = "!commands";
+        public const string EXIT = "!exit";
+    }
+}
